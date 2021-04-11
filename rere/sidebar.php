95c72317c1551d83bffbc1443ac71eaab3c4376f
yusuffadillah1701 <72794413+yusuffadillah1701@users.noreply.github.com>
2021-04-11 23:15:30 +0700

    Add files via upload

---
+++ rere/sidebar.php
@@ -0,0 +1,60 @@
+    <div class="col-md-3">
+        <div class="card">
+            <div class="media mt-2">
+                <div style="padding:10px;">
+                    <h3 style="margin:0; color: #333333; font-size:18px; font-family: Helvetica;"><i class="fa fa-newspaper"></i> &nbsp;Berita terbaru</h3>
+                </div>
+            </div>
+            <div class="media mt-4 mb-3">
+                <img src="img/Berita/nf.png" class="mr-3 ml-2" width="60" height="60" alt="...">
+                <div class="media-body">
+                    <h5 style="font-size: 15px;"><a href="https://www.liputan6.com/otomotif/read/4525244/top-3-berita-hari-ini-keyless-motor-bisa-dibobol-dan-tips-standar-samping">
+                            Top 3 Berita Hari Ini: Keyless Motor Bisa Dibobol dan Tips Standar Samping</a></h5>
+                    <p class="text-muted mt-1" style="font-size: 12px;"><span><i class="fas fa-clock"></i> 06 Apr 2021, 20:06 WIB</span></p>
+                </div>
+            </div>
+            <div class="media mb-3">
+                <img src="img/Berita/nf.png" class="mr-3 ml-2" width="60" height="60" alt="...">
+                <div class="media-body">
+                    <h5 style="font-size: 15px;"><a href="https://www.liputan6.com/bisnis/read/4522473/daftar-lengkap-29-mobil-yang-dapat-diskon-pajak-minat-beli-yang-mana">
+                            Daftar Lengkap 29 Mobil yang Dapat Diskon Pajak, Minat Beli yang Mana?</a></h5>
+                    <p class="text-muted mt-1" style="font-size: 12px;"><span><i class="fas fa-clock"></i> 03 Apr 2021, 13:45 WIB</span></p>
+                </div>
+            </div>
+            <div class="media mb-3">
+                <img src="img/Berita/nf.png" class="mr-3 ml-2" width="60" height="60" alt="...">
+                <div class="media-body">
+                    <h5 style="font-size: 15px;"><a href="https://news.detik.com/berita/d-5524221/wagub-dki-soal-massa-beratribut-hmi-demo-balkot-jangan-asal-bicara-korupsi?tag_from=wp_nhl_3">
+                            Wagub DKI soal Massa Beratribut HMI Demo Balkot: Jangan Asal Bicara Korupsi</a></h5>
+                    <p class="text-muted mt-1" style="font-size: 12px;"><span><i class="fas fa-clock"></i> Rabu, 07 Apr 2021 22:40 WIB</span></p>
+                </div>
+            </div>
+            <div class="media mb-3">
+                <img src="img/Berita/nf.png" class="mr-3 ml-2" width="60" height="60" alt="...">
+                <div class="media-body">
+                    <h5 style="font-size: 15px;"><a href="https://news.detik.com/berita/d-5524206/pecahnya-demokrat-dan-skenario-suara-pemilu-2024?tag_from=wp_nhl_12">
+                            Pecahnya Demokrat dan Skenario Suara Pemilu 2024 </a></h5>
+                    <p class="text-muted mt-1" style="font-size: 13px;"><span><i class="fas fa-clock"></i> Rabu, 07 Apr 2021 22:12 WIB</span></p>
+                </div>
+            </div>
+            <div class="media mb-4">
+                <img src="img/Berita/nf.png" class="mr-3 ml-2" width="60" height="60" alt="...">
+                <div class="media-body">
+                    <h5 style="font-size: 15px;"><a href="https://news.detik.com/berita/d-5524203/penelusuran-jaringan-bomber-gereja-merambah-jagakarsa?tag_from=wp_nhl_15">
+                            Penelusuran Jaringan Bomber Gereja Merambah Jagakarsa </a></h5>
+                    <p class="text-muted mt-1" style="font-size: 13px;"><span><i class="fas fa-clock"></i> Rabu, 07 Apr 2021 22:04 WIB</span></p>
+                </div>
+            </div>
+            <h6 align="center"><a href="https://news.detik.com/">Berita Lainnya Bisa Dilihat Disini</a></h6>
+        </div>
+        <div class="list-group mb-5 mt-5">
+            <a href="" class="list-group-item list-group-item-action active" aria-current="true">
+                The current link item
+            </a>
+            <a href="" class="list-group-item list-group-item-action">A second link item</a>
+            <a href="" class="list-group-item list-group-item-action">A third link item</a>
+            <a href="" class="list-group-item list-group-item-action">A fourth link item</a>
+            <a href="" class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">A disabled link item</a>
+        </div>
+    </div>
+    </div>
